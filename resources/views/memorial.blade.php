5b4425bd957480a626c64198e6916f0104917bee
AndyWynham <Wlad.Syrovatskiy@mail.ru>
2022-07-07 20:30:28 +0300

    configured for mobile devices

--- resources/views/memorial.blade.php
+++ resources/views/memorial.blade.php
@@ -5,11 +5,17 @@
 @endsection
 
 @section('content')
-<div class="container col-md-9 mx-auto" id="container">
-    <img src="/images/memorials_photo/volzhsky_memorial.jpg" id="memorial-photo" class="rounded img-fluid">
-    <span id="memrl-descrip" style="font-size: 20px;">
-        <?= file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/memorials_descriptions/volzhsky_memorial_description.txt")?>
-    </span>
+<div class="container" id="container">
+    <div class="row align-items-center mb-5">
+        <div class="col-lg-4 col-sm-12 text-center">
+            <img src="/images/memorials_photo/volzhsky_memorial.jpg" id="memorial-photo" class="rounded">
+        </div>
+        <div class="col-lg-8 col-sm-12 mb-sm-5 mt-5">
+            <span id="memrl-descrip" style="font-size: 20px;">
+                <?= file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/memorials_descriptions/volzhsky_memorial_description.txt")?>
+            </span>
+        </div>
+    </div>
 </div>
 @endsection
 
--- resources/views/soldiersList.blade.php
+++ resources/views/soldiersList.blade.php
@@ -5,19 +5,19 @@
 @endsection
 
 @section('content')
-    <div class="container">
+    <div class="container" id="container">
         <div class="row">
-            <div class="col-12">
+            <div class="col-12" id="snf-col">
                 <h2 class="text-center text-uppercase srch-title">Результаты поиска</h2>
             </div>
         </div>
         @foreach ($soldiers as $soldier)
-        <div class="row justify-content-md-center align-items-center mb-5 srch-row">
-            <div class="col col-lg-2 col-sm-12">
+        <div class="row row-cols-auto justify-content-md-center align-items-center mt-5 mx-auto srch-row">
+            <div class="col-lg-2 col-sm-12" id="snf-col">
                 @if ($soldier->photo !== NULL)
-                    <img src="/images/soldiers_photo/{{ $soldier->photo }}" id="soldier-icon" class="img-fluid rounded">
+                    <img src="/images/soldiers_photo/{{ $soldier->photo }}" id="soldier-icon" class="rounded">
                 @else
-                    <img src="/images/soldiers_photo/default.jpg" id="soldier-icon" class="img-fluid rounded">
+                    <img src="/images/soldiers_photo/default.jpg" id="soldier-icon" class="rounded">
                 @endif
             </div>
             <div class="col-md-auto">
